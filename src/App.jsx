c1364ff8fcaf6dfe0aea02bb694786b801286050
Tayyaba Taimur <tayyabataimur@main>
2024-06-28 13:32:54 +0100

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Root from "./routes/root";
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Root />} />
       </Routes>
-    </Router>
+    </>
   );
 };
 
